31bb598f049747325215e01be84d9cb57bf4c88c
effrancina <f.v.menichetti@gmail.com>
2022-11-22 22:35:56 +0000

    2 mvp finished

---
+++ models/park.js
@@ -0,0 +1,29 @@
+const Dinosaur = require('../specs/dinosaur_spec.js');
+
+const Park = function (name, tprice, dinosaurs = []) {
+    this.name = name;
+    this.tprice = tprice;
+    this.dinosaurs = dinosaurs;
+  }
+
+Park.prototype.addDinosaur = function (dinosaur) {
+    this.dinosaurs.push(dinosaur);
+};
+
+Park.prototype.removeDinosaur = function (dinosaur) {
+    const indexOfDinosaur = this.dinosaurs.indexOf(dinosaur);
+    this.dinosaurs.shift(indexOfDinosaur, 0);
+};
+
+Park.prototype.popularDinosaur = function () {
+    let mostPopularDinosaur;
+    for (let i=0; i > mostPopularDinosaur || mostPopularDinosaur===None; i===mostPopularDinosaur) {
+        console.log(mostPopularDinosaur);
+    }
+};
+
+
+
+
+
+module.exports = Park;
